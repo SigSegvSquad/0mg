2fb1e866d1908c389bbc8ed7572dfc5dabd2166f
OverPoweredDev <omkar.prabhune19@vit.edu>
2021-03-02 15:34:16 +0530

    failed fab added

---
+++ lib/screens/home/components/fab.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+
+import 'package:shop_app/screens/cart/cart_screen.dart';
+import 'package:shop_app/screens/subscription/subscription_screen.dart';
+
+class FancyFab extends StatefulWidget {
+  // final Function() onPressed;
+  // final String tooltip;
+  // final IconData icon;
+  //
+  // FancyFab({this.onPressed, this.tooltip, this.icon});
+
+  @override
+  _FancyFabState createState() => _FancyFabState();
+}
+
+class _FancyFabState extends State<FancyFab> with SingleTickerProviderStateMixin {
+  bool isOpened = false;
+  AnimationController _animationController;
+  Animation<Color> _buttonColor;
+  Animation<double> _animateIcon;
+  Animation<double> _translateButton;
+  Curve _curve = Curves.easeOut;
+  double _fabHeight = 10.0;
+
+  @override
+  initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500))
+          ..addListener(() {
+            setState(() {});
+          });
+    _animateIcon =
+        Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
+    _buttonColor = ColorTween(
+      begin: Colors.blue,
+      end: Colors.red,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        0.00,
+        1.00,
+        curve: Curves.linear,
+      ),
+    ));
+    _translateButton = Tween<double>(
+      begin: _fabHeight,
+      end: -14.0,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        0.0,
+        0.75,
+        curve: _curve,
+      ),
+    ));
+    super.initState();
+  }
+
+  @override
+  dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  animate() {
+    if (!isOpened) {
+      _animationController.forward();
+    } else {
+      _animationController.reverse();
+    }
+    isOpened = !isOpened;
+  }
+
+  Widget subscription() {
+    return Container(
+      child: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, SubscriptionScreen.routeName);
+        },
+        tooltip: 'Subscriptions',
+        child: Icon(Icons.access_time_rounded),
+      ),
+    );
+  }
+
+  Widget cart() {
+    return Container(
+      child: FloatingActionButton(
+        onPressed:  () {
+          Navigator.pushNamed(context, CartScreen.routeName);
+        },
+        tooltip: 'Cart',
+        child: Icon(Icons.shopping_cart_outlined),
+      ),
+    );
+  }
+
+  Widget toggle() {
+    return Container(
+      child: FloatingActionButton(
+        backgroundColor: _buttonColor.value,
+        onPressed: animate,
+        tooltip: 'Toggle',
+        child: AnimatedIcon(
+          icon: AnimatedIcons.menu_close,
+          progress: _animateIcon,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Transform(
+          transform: Matrix4.translationValues(
+            0.0,
+            _translateButton.value * 3.0,
+            0.0,
+          ),
+          child: subscription(),
+        ),
+        Transform(
+          transform: Matrix4.translationValues(
+            0.0,
+            _translateButton.value,
+            0.0,
+          ),
+          child: cart(),
+        ),
+        toggle(),
+      ],
+    );
+  }
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/components/coustom_bottom_nav_bar.dart';
 import 'package:shop_app/enums.dart';
+import 'package:shop_app/screens/home/components/fab.dart';
 import 'package:shop_app/screens/sidebar/sidebar.dart';
 import 'components/body.dart';
 
@@ -9,6 +10,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // floatingActionButton: new FancyFab(),
       drawer: NavDrawer(),
       body: Body(),
     );
