8c90517189a314e3db9dcb4626f7da5830a2769f
OverPoweredDev <omkar.prabhune19@vit.edu>
2021-03-15 10:39:11 +0530

    logout

--- lib/screens/sidebar/sidebar.dart
+++ lib/screens/sidebar/sidebar.dart
@@ -1,12 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:shop_app/screens/profile/profile_screen.dart';
-import 'package:shop_app/screens/home/home_screen.dart';
 import 'package:shop_app/models/User.dart';
+import 'package:shop_app/screens/home/home_screen.dart';
+import 'package:shop_app/screens/profile/profile_screen.dart';
+import 'package:shop_app/screens/sign_in/sign_in_screen.dart';
 import 'package:shop_app/screens/subscription/subscription_screen.dart';
 
 class NavDrawer extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+
+    void logout() {
+      FirebaseAuth.instance.signOut();
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => (SignInScreen())));
+    }
+
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
@@ -15,32 +25,29 @@ class NavDrawer extends StatelessWidget {
             width: double.infinity,
             height: 150,
             child: DecoratedBox(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end:
-                  Alignment(0.8, 0.0), // 10% of the width, so there are ten blinds.
-                  colors: [
-                    const Color(0xFF4A3298),
-                    const Color(0xFF4A3298)
-                  ]
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment(0.8, 0.0),
+                      // 10% of the width, so there are ten blinds.
+                      colors: [
+                        const Color(0xFF4A3298),
+                        const Color(0xFF4A3298)
+                      ]),
                 ),
-                ),
-              child:
-              Align(
-                alignment: Alignment.centerLeft,
-              child:
-              Container(
-                margin: EdgeInsets.all(20),
-                  child:Text(
-                username,
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              )))),
-            ),
+                child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Container(
+                        margin: EdgeInsets.all(20),
+                        child: Text(
+                          username,
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )))),
+          ),
           ListTile(
             leading: Icon(Icons.home),
             title: Text('Home'),
@@ -49,12 +56,14 @@ class NavDrawer extends StatelessWidget {
           ListTile(
             leading: Icon(Icons.verified_user),
             title: Text('Profile'),
-            onTap: () => {Navigator.pushNamed(context, ProfileScreen.routeName)},
+            onTap: () =>
+                {Navigator.pushNamed(context, ProfileScreen.routeName)},
           ),
           ListTile(
             leading: Icon(Icons.access_time_rounded),
             title: Text('Subscription'),
-            onTap: () => {Navigator.pushNamed(context, SubscriptionScreen.routeName)},
+            onTap: () =>
+                {Navigator.pushNamed(context, SubscriptionScreen.routeName)},
           ),
           ListTile(
             leading: Icon(Icons.settings),
@@ -69,8 +78,8 @@ class NavDrawer extends StatelessWidget {
           ListTile(
             leading: Icon(Icons.exit_to_app),
             title: Text('Logout'),
-            onTap: () => {Navigator.of(context).pop()},
-          ),
+            onTap: () => {logout()},
+          )
         ],
       ),
     );
